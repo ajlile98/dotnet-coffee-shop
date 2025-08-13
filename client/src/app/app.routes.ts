dfc21b73a649fcb29d5253282a10d1187d621380
Andy <ajlile98@gmail.com>
2025-08-13 12:03:06 -0400

    implement routing guards, and splash page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { CustomerList } from '../features/customers/customer-list/customer-list';
+import { CustomerDetailed } from '../features/customers/customer-detailed/customer-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { Menu } from '../features/menu/menu';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'customers', component: CustomerList},
+            {path: 'customers/:id', component: CustomerDetailed},
+            {path: 'lists', component: Lists},
+            {path: 'messages', component: Messages},
+            {path: 'menu', component: Menu},
+        ]
+    },
+    {path: '**', component: Home},
+];
---
+++ client/src/features/customers/customer-detailed/customer-detailed.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-detailed',
+  imports: [],
+  templateUrl: './customer-detailed.html',
+  styleUrl: './customer-detailed.css'
+})
+export class CustomerDetailed {
+
+}
